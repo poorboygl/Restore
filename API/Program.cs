00b5726dac42cfb7e6d69684de8a6d7d216f2252
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2025-09-30 21:35:49 +0700

    ch02-007 Creating the Entity Framework DbContext class

--- API/Program.cs
+++ API/Program.cs
@@ -1,9 +1,17 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddDbContext<StoreContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+
 
 var app = builder.Build();
 
