524cb6c48a34f54b2938fc5a8face5c7e4543701
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2025-10-31 14:36:52 +0700

    ch02-011 Adding a Products API Controller

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+using System;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[Route("api/[controller]")] //? https://localhost:5001/api
+[ApiController]
+public class ProductsController(StoreContext context) : ControllerBase
+{
+
+    [HttpGet]
+    public ActionResult<List<Product>> GetProducts()
+    {
+        return context.Products.ToList();
+    }
+
+    [HttpGet("{id}")] //api/products/2
+    public ActionResult<Product> GetProduct(int id)
+    {
+        var product = context.Products.Find(id);
+
+        if (product == null) return NotFound();
+
+        return product;
+    }
+}
