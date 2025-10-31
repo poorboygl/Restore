282d80f14172a7e7442beae1024a87bceb1ec7ca
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2025-10-31 14:00:45 +0700

    ch02-009 Adding Seed Data to populate the database with dummy data

--- API/Entities/Product.cs
+++ API/Entities/Product.cs
@@ -7,7 +7,7 @@ public class Product
     public int Id { get; set; }
     public required string Name { get; set; }
     public required string Description { get; set; }
-    public long price { get; set; }
+    public long Price { get; set; }
     public required string PictureUrl { get; set; }
     public required string Type { get; set; }
     public required string Brand { get; set; }
